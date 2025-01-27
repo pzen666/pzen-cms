a7cd018659f2a70797b8f5bd6642787fac14f859
cheng <896911036@163.com>
2025-01-27 09:50:05 +0800

    初始化

---
+++ src/main/java/com/pzen/pzencms/PzenCmsApplication.java
@@ -0,0 +1,13 @@
+package com.pzen.pzencms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PzenCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PzenCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pzen/pzencms/PzenCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pzen.pzencms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PzenCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
