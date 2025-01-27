d8f56b2d63275b1f65c110a85096a35f789118ec
cheng <896911036@163.com>
2025-01-27 17:26:08 +0800

    Ebean查询demo

---
+++ server/src/java/com/pzen/server/service/impl/VideoServiceImpl.java
@@ -0,0 +1,44 @@
+package com.pzen.server.service.impl;
+
+import com.pzen.entity.Video;
+import com.pzen.server.service.VideoService;
+import io.ebean.DB;
+
+import java.util.List;
+import java.util.Optional;
+
+public class VideoServiceImpl extends EbeanRepositoryImpl<Video, String>
+        implements VideoService {
+
+
+    public VideoServiceImpl(Class<VideoService> entityType) {
+        super(Video.class);
+    }
+
+    @Override
+    public List<Video> findByVideoBatchId(String batchId) {
+        return DB.find(Video.class)
+                .where()
+                .eq("video_id", batchId)
+                .findList();
+    }
+
+    @Override
+    public Optional<Video> findByVideoId(String paymentId) {
+        return Optional.ofNullable(DB.find(Video.class)
+                .where()
+                .eq("paymentId", paymentId)
+                .findOne());
+    }
+
+    @Override
+    public List<Video> findAllByBatchIdAndStatus(String batchId, List<String> statuses) {
+        return DB.find(Video.class)
+                .where()
+                .and()
+                .eq("amdBatchPayment.batchId", batchId)
+                .in("status", statuses)
+                .endAnd()
+                .findList();
+    }
+}
