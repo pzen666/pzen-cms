31c925a6c6bf748abb051739515ca86ecf39ffeb
cheng <896911036@163.com>
2025-02-04 19:21:47 +0800

    修改

--- server/src/java/com/pzen/server/config/WebConfig.java
+++ server/src/java/com/pzen/server/config/WebConfig.java
@@ -1,20 +1,11 @@
 package com.pzen.server.config;
 
-//import com.pzen.server.filter.DecryptRequestInterceptor;
-//import com.pzen.server.filter.EncryptResponseInterceptor;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-//    @Autowired
-//    private EncryptResponseInterceptor encryptResponseInterceptor;
-//
-//    @Autowired
-//    private DecryptRequestInterceptor decryptRequestInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -24,12 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(false);
     }
-
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(encryptResponseInterceptor).addPathPatterns("/**");
-//        registry.addInterceptor(decryptRequestInterceptor).addPathPatterns("/**");
-//    }
 
 
 }
