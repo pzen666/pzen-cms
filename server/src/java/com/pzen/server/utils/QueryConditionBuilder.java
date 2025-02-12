213bad69147f4157b248b478a2588b3033e7b54b
cheng <896911036@163.com>
2025-02-12 17:40:25 +0800

    分页查询增加常用sql查询方式类型

--- server/src/java/com/pzen/server/utils/QueryConditionBuilder.java
+++ server/src/java/com/pzen/server/utils/QueryConditionBuilder.java
@@ -6,6 +6,7 @@ import io.ebean.ExpressionList;
 import io.ebean.Query;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 public class QueryConditionBuilder {
@@ -20,53 +21,53 @@ public class QueryConditionBuilder {
                 Object value = condition.getValue();
                 if (value != null && StringUtils.isNotEmpty(String.valueOf(value)) && !String.valueOf(value).equals("null")) {
                     switch (type) {
-                        case "eq":
+                        case "eq"://等于
                             expressionList.eq(fieldName, value);
                             break;
-                        case "ne":
+                        case "ne"://不等于
                             expressionList.ne(fieldName, value);
                             break;
-                        case "gt":
+                        case "gt"://大于
                             expressionList.gt(fieldName, value);
                             break;
-                        case "gte":
-                            expressionList.gtIfPresent(fieldName, value);
+                        case "gte"://大于等于
+                            expressionList.gt(fieldName, value).eq(fieldName, value);
                             break;
-                        case "lt":
+                        case "lt"://小于
                             expressionList.lt(fieldName, value);
                             break;
-                        case "lte":
-                            expressionList.ltIfPresent(fieldName, value);
+                        case "lte"://小于等于
+                            expressionList.lt(fieldName, value).eq(fieldName, value);
                             break;
-                        case "lik":
+                        case "lik"://模糊查询
                             expressionList.like(fieldName, "%" + value + "%");
                             break;
-                        case "startSwitch":
+                        case "startSwitch"://字符串末尾  模糊查询
                             expressionList.like(fieldName, value + "%");
                             break;
-                        case "endSwitch":
+                        case "endSwitch": //字符串开头 模糊查询
                             expressionList.like(fieldName, "%" + value);
                             break;
-                        case "in":
+                        case "i":// in 在其中
                             if (value instanceof Collection) {
                                 expressionList.in(fieldName, (Collection<?>) value);
                             }
                             break;
-                        case "notin":
+                        case "notIn"://不在其中
                             if (value instanceof Collection) {
                                 expressionList.notIn(fieldName, (Collection<?>) value);
                             }
                             break;
-                        case "isNull":
+                        case "isNull"://为空
                             expressionList.isNull(fieldName);
                             break;
-                        case "isNotNull":
+                        case "isNotNull"://不为空
                             expressionList.isNotNull(fieldName);
                             break;
-                        case "between":
-                            if (value instanceof Object[] rangeValues) {
-                                if (rangeValues.length == 2) {
-                                    expressionList.between(fieldName, rangeValues[0], rangeValues[1]);
+                        case "between"://范围查询
+                            if (value instanceof List<?> rangeValues) {
+                                if (rangeValues.size() == 2) {
+                                    expressionList.between(fieldName, rangeValues.get(0), rangeValues.get(1));
                                 }
                             }
                             break;
