5d285558b8a5bbaa6c4f482e245cc4afa2ccfb42
cheng <896911036@163.com>
2025-02-12 17:21:56 +0800

    修改增加xxs sql注入拦截后 前端查询条件方式  具体请查看com.pzen.server.utils.QueryConditionBuilder buildConditions()方法

--- server/src/java/com/pzen/server/utils/QueryConditionBuilder.java
+++ server/src/java/com/pzen/server/utils/QueryConditionBuilder.java
@@ -1,9 +1,11 @@
 package com.pzen.server.utils;
 
 import com.pzen.utils.Condition;
+import com.pzen.utils.StringUtils;
 import io.ebean.ExpressionList;
 import io.ebean.Query;
 
+import java.util.Collection;
 import java.util.Map;
 
 public class QueryConditionBuilder {
@@ -16,22 +18,63 @@ public class QueryConditionBuilder {
                 Condition condition = entry.getValue();
                 String type = condition.getType();
                 Object value = condition.getValue();
-                switch (type) {
-                    case "equals":
-                        expressionList.eq(fieldName, value);
-                        break;
-                    case "like":
-                        expressionList.like(fieldName, "%" + value + "%");
-                        break;
-                    case "gt":
-                        expressionList.gt(fieldName, value);
-                        break;
-                    case "lt":
-                        expressionList.lt(fieldName, value);
-                        break;
-                    // 可以添加更多条件类型
-                    default:
-                        break;
+                if (value != null && StringUtils.isNotEmpty(String.valueOf(value)) && !String.valueOf(value).equals("null")) {
+                    switch (type) {
+                        case "eq":
+                            expressionList.eq(fieldName, value);
+                            break;
+                        case "ne":
+                            expressionList.ne(fieldName, value);
+                            break;
+                        case "gt":
+                            expressionList.gt(fieldName, value);
+                            break;
+                        case "gte":
+                            expressionList.gtIfPresent(fieldName, value);
+                            break;
+                        case "lt":
+                            expressionList.lt(fieldName, value);
+                            break;
+                        case "lte":
+                            expressionList.ltIfPresent(fieldName, value);
+                            break;
+                        case "lik":
+                            expressionList.like(fieldName, "%" + value + "%");
+                            break;
+                        case "startSwitch":
+                            expressionList.like(fieldName, value + "%");
+                            break;
+                        case "endSwitch":
+                            expressionList.like(fieldName, "%" + value);
+                            break;
+                        case "in":
+                            if (value instanceof Collection) {
+                                expressionList.in(fieldName, (Collection<?>) value);
+                            }
+                            break;
+                        case "notin":
+                            if (value instanceof Collection) {
+                                expressionList.notIn(fieldName, (Collection<?>) value);
+                            }
+                            break;
+                        case "isNull":
+                            expressionList.isNull(fieldName);
+                            break;
+                        case "isNotNull":
+                            expressionList.isNotNull(fieldName);
+                            break;
+                        case "between":
+                            if (value instanceof Object[] rangeValues) {
+                                if (rangeValues.length == 2) {
+                                    expressionList.between(fieldName, rangeValues[0], rangeValues[1]);
+                                }
+                            }
+                            break;
+                        default:
+                            // 处理未知的条件类型
+                            break;
+                    }
+
                 }
             }
         }
